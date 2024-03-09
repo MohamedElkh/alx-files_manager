82f9c821014a92eb02dccb1a90b3ddc37ff09656
mohamed <mohamedkharashy27@yahoo.com>
2024-03-09 00:56:35 -0800

    1

--- utils/db.js
+++ utils/db.js
@@ -2,13 +2,14 @@ import { MongoClient } from 'mongodb';
 
 const HOST = process.env.DB_HOST || 'localhost';
 const PORT = process.env.DB_PORT || 27017;
-const DATABASE = process.env.DB_DATABASE || 'files_manager';
 
+const DATABASE = process.env.DB_DATABASE || 'files_manager';
 const url = `mongodb://${HOST}:${PORT}`;
 
 class DBClient {
   constructor() {
     this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
+
     this.client.connect().then(() => {
       this.db = this.client.db(`${DATABASE}`);
     }).catch((err) => {
@@ -22,14 +23,16 @@ class DBClient {
 
   async nbUsers() {
     const users = this.db.collection('users');
-    const usersNum = await users.countDocuments();
-    return usersNum;
+
+    const usx = await users.countDocuments();
+    return usx;
   }
 
   async nbFiles() {
     const files = this.db.collection('files');
-    const filesNum = await files.countDocuments();
-    return filesNum;
+
+    const filx = await files.countDocuments();
+    return filx;
   }
 }
 
